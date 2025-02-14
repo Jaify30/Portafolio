38f38a5c958b37943a66e75ba39007b1ad82d9f3
Jaify30 <josuevillavicenciodev@gmail.com>
2025-02-14 14:19:34 -0300

    Se le agrega el script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Ajusta el desplazamiento si tienes un header fijo
+                behavior: 'smooth'
+            });
+        }
+    });
+});
